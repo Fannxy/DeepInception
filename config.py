86dc30e1d00db6fe61c7e378c5bd5da4f262f69f
Fannxy <fxy23@mails.tsinghua.edu.cn>
2024-11-11 18:34:02 +0800

    update config for tsinghua environment

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-ROOT_PATH = './'
-VICUNA_PATH = f"{ROOT_PATH}/vicuna-7b-v1.5"
-LLAMA_PATH = f"{ROOT_PATH}/Llama-2-7b-hf"
-FALCON_PATH = f"{ROOT_PATH}/falcon-7b-instruct"
+ROOT_PATH = '/share/fanxy/Models'
+# VICUNA_PATH = f"{ROOT_PATH}/vicuna-7b-v1.5"
+# LLAMA_PATH = f"{ROOT_PATH}/Llama-2-7b-hf"
+# FALCON_PATH = f"{ROOT_PATH}/falcon-7b-instruct"
+VICUNA_PATH = "lmsys/vicuna-7b-v1.5-16k"
+LLAMA_PATH = "meta-llama/Llama-2-7b-chat-hf"
+FALCON_PATH = "tiiuae/falcon-7b-instruct"
+
 
 ATTACK_TEMP = 1
 TARGET_TEMP = 0
